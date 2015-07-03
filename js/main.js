fc91b56003bc441d868f06ec42d2d445e1bcd147
dis-zephyro <zephyro@yandex.ru>
2015-07-04 01:38:37 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Slider
+
+$('.partners-slide').slick({
+    arrows: false,
+    autoplay: false,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1
+});
+
+$('.partners-nav.prev').click(function(){
+    $('.partners-slide').slick('slickPrev');
+});
+
+$('.partners-nav.next').click(function(){
+    $('.partners-slide').slick('slickNext');
+});
+
+
+$('.winner ul').slick({
+    arrows: false,
+    autoplay: false,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+$('.winner-nav.prev').click(function(){
+    $('.winner ul').slick('slickPrev');
+});
+
+$('.winner-nav.next').click(function(){
+    $('.winner ul').slick('slickNext');
+});
+
+//---
